5caab4a3821c707e2ab872a9cd6a742c4104d8dd
Ocean927 <haogu2580@gmail.com>
2023-03-23 21:04:54 +0800

    first commit

---
+++ module01/src/com/atguigu/guliaccount/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.guliaccount;
+
+/**
+ * Classname:GitTest
+ * Package:com.atguigu.guliaccount
+ * Description:
+ *
+ * @author
+ * @Create 2023/3/23 20:48
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好！");
+    }
+}
---
+++ module01/src/com/atguigu/guliaccount/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.atguigu.guliaccount;
+
+/**
+ * Classname:HelloWorld
+ * Package:GuliAccount
+ * Description:
+ *
+ * @author
+ * @Create 2023/3/21 20:23
+ * @Version 1.0
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("你好" + i);
+        }
+    }
+}   
