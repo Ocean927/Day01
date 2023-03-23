167cd6c23c9b7c509d4201e2c5f037f32151bcf6
Ocean927 <haogu2580@gmail.com>
2023-03-23 21:12:24 +0800

    second commit

--- module01/src/com/atguigu/guliaccount/GitTest.java
+++ module01/src/com/atguigu/guliaccount/GitTest.java
@@ -5,11 +5,12 @@ package com.atguigu.guliaccount;
  * Package:com.atguigu.guliaccount
  * Description:
  *
- * @author
- * @Create 2023/3/23 20:48
+ * @author gh
+ * @create 2023/3/23 20:48
  **/
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好！");
+        System.out.println("hello Git again!");
     }
 }
